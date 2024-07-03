940c527744a38d4948ce4619da19c0fec9f6a3eb
Infinite-Learner <152425498+Infinite-Learner@users.noreply.github.com>
2024-07-03 16:07:45 +0530

    new Files Updated

---
+++ StringPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace CS4
+{
+    public class StringPattern {
+        public static void Main()
+        {
+            string s = "123456789";
+            int k = 1;
+            for (int i = 0; i < (s.Length + 1) / 2; i++)
+            {
+                for (int j = i; j < 2*s.Length; j++)
+                {
+                    if (j <= 2 * i)
+                    {
+                        k = s[j] - '0';
+                        Console.Write(k + " ");
+                    }
+                    else
+                    {
+                        --k;
+                        if (k > i)
+                        {
+                            Console.Write(k + " ");
+                        }
+                    }  
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ VirtualandOverride/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace CS5A
+{
+    public class Virtual
+    {
+        int a, b;
+        public virtual void getValue()
+        {
+            Console.WriteLine("Base Class Method is Invoked...");
+            Console.Write("Enter Value of a : ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter Value of b : ");
+            b = Int32.Parse(Console.ReadLine());
+        }
+        public virtual void disp()
+        {
+            Console.WriteLine("Base Class Data Members are a = {0} , b = {1}.", a, b);
+        }
+    }
+    public class VirtualExt : Virtual
+    {
+        int a, b;
+        public override void getValue()
+        {
+            Console.WriteLine("Derive Class Method is Invoked...");
+            Console.Write("Enter Value of a : ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter Value of b : ");
+            b = Int32.Parse(Console.ReadLine());
+        }
+        public override void disp()
+        {
+            Console.WriteLine("Derive Class Data Members are a = {0} , b = {1}.", a, b);
+        }
+    }
+    public class VirtualOverride
+    {
+        public static void Main()
+        {
+            Virtual obj = new Virtual();
+            obj.getValue();
+            obj.disp();
+            //Overriding Derive class method and Base class is Overriden.
+            obj = new VirtualExt();
+            obj.getValue();
+            obj.disp();
+        }
+    }
+}
