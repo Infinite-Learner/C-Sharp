b5e005c97c4e32f33ba7eb6b00d3e60d9177136a
Infinite-Learner <msdeepakpious@gmail.com>
2024-07-04 10:57:43 -0700

    Updated Abstract

--- 6.b.Abstract/Program.cs
+++ 6.b.Abstract/Program.cs
@@ -3,16 +3,35 @@ namespace CS6B
 {
     public abstract class Abstract
     {
-       public abstract void getValue(int n);
-    }
-     class NonAbstract : Abstract {
         public int[] a;
-        public override void getValue(int n)
+        public abstract void getValue(int n);
+        public void show()
         {
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Array Created and Elements are ... ");
+            for (int i = 0; i < a.Length; i++)
             {
-                a[i] = Int32.Parse(Console.ReadLine());
+                Console.Write(" Element {0} : {1}\n", i + 1, a[i]);
             }
+        }
+        public class AbstractImp : Abstract
+        {
+            public override void getValue(int n)
+            {
+                a = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Enter Element {0} : ", i + 1);
+                    a[i] = Int32.Parse(Console.ReadLine());
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            AbstractImp ab = new AbstractImp();
+            Console.WriteLine("Implementation of Array using Abstract Class ... ");
+            Console.Write("Enter Size of Array : ");
+            ab.getValue(Int32.Parse(Console.ReadLine()));
+            ab.show();
         }
     }
 }
