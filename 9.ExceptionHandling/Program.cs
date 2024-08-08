dc9bcf0fe67f70113b9278d2334d4ca04ab65048
Infinite-Learner <msdeepakpious@gmail.com>
2024-08-09 04:02:55 +0530

    update

--- 9.ExceptionHandling/Program.cs
+++ 9.ExceptionHandling/Program.cs
@@ -1,40 +1,38 @@
 ﻿using System;
 namespace CS9 {
     class ExceptionHandling{
-        public static int Add(int a,int b)
+        public static object Add(int a,int b)
         {
-            try
+            int sum = 0;
+            if (a == 0 && b == 0)
             {
-                if (b < 0 || a < 0)
-                {
-                    throw new ArgumentException();
-                    
-                }
-                return a + b;
+                ArgumentException e = new ArgumentException("any one paramters value should be 1 or greater");
+                return e;
             }
-            catch (ArgumentException) {
-            
-                Console.WriteLine("Paramter value should be Positive");
-                return (a < 0 ? a : b);
-            }
+            sum = a + b;
+            return sum;
         }
        public static void Main(string[] args) {
             try
             {
-                int div = 0;
-                char[] Name;
+                char[] Name = new char[15];
+                int m=0, n=0;
                 Console.Write("Enter Name(Don't exceed 15 characters) :");
                 Name = Console.ReadLine().ToCharArray();
                 if (Name.Length > 15)
                 {
                     throw new IndexOutOfRangeException();
                 }
-                    Console.Write("Welcome : ");
-                    Console.WriteLine(Name);
-                Console.Write("Enter a number : ");
-                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Welcome : ");
+                Console.WriteLine(Name);
+                Console.Write("Enter a number 1 : ");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter a number 2 : ");
+                m = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The Sum Of {0}+{1} : {2}", m, n, Add(m, n));
+                Console.Write("The {0}/0 Divided by 0 goes : ",m);
+                Console.WriteLine(m / 0);
 
-                Console.WriteLine(n / div);
                 
             }
             catch (IndexOutOfRangeException)
@@ -43,26 +41,18 @@ namespace CS9 {
             }
             catch (DivideByZeroException)
             {
-                Console.WriteLine("Cannot Divide By Zero ");
-                int a = 10, b = 20;
-                Add(a, -b);
-                Console.WriteLine();
-
+                Console.WriteLine(new DivideByZeroException("Cannot Divide By Zero "));
             }
             catch (NotFiniteNumberException)
             {
                 Console.WriteLine("It is Not-a-Number(NAN)");
             }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("Parameter Specification Mismatch");
-            }
             catch (FormatException) {
-                Console.WriteLine("The Entered Input Format is incorrect");
+              Console.WriteLine("The Entered Input Format is invalid");
             }
             finally
             {
-                Console.WriteLine("Illustration of Exception Handling methods with Multiple catch and throw Keyword");
+                Console.WriteLine("Illustration of Exception Handling methods with\n1.throw Keyword\n2.try-catch block\n3.finally");
             }
         }
     }
